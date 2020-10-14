89b8be4f5c71c1a2d492254b984a3fc41b0f9341
JuniorBaymax <cooladitya953@gmail.com>
2020-10-15 00:43:28 +0530

    Refactor code and reduse Junk code in Contact component

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,6 +1,33 @@
 import React from "react";
 
 export default function Contact() {
+  const details = [
+    {
+      id: 1,
+      heading: "Address",
+      subHeading: "198 West 21th Street, Suite 721 New York NY 10016",
+      icon: "map-signs",
+    },
+    {
+      id: 2,
+      heading: "Contact",
+      subHeading: "+91 123456789",
+      icon: "phone",
+    },
+    {
+      id: 3,
+      heading: "email Address",
+      subHeading: "support@trust.com",
+      icon: "paper-plane",
+    },
+    {
+      id: 4,
+      heading: "website",
+      subHeading: "trust@career.com",
+      icon: "globe-asia",
+    },
+  ];
+
   return (
     <div className="container">
       <div className="row justify-content-center mt-5 mb-5 pb-3">
@@ -14,50 +41,21 @@ export default function Contact() {
         </div>
       </div>
       <div className="row d-flex contact__section mb-5">
-        <div className="col-md-6 col-lg-3 d-flex">
-          <div className="align-self-stretch box p-4  text-center">
-            <div className="contact__section-icon d-flex align-items-center justify-content-center">
-              <span className="icon-map-sign">
-                <i className="fas fa-map-signs"></i>
-              </span>
+        {details.map(({ id, heading, subHeading, icon }) => {
+          return (
+            <div key={id} className="col-md-6 col-lg-3 d-flex">
+              <div className="align-self-stretch box p-4  text-center">
+                <div className="contact__section-icon d-flex align-items-center justify-content-center">
+                  <span className="icon-map-sign">
+                    <i className={`fas fa-${icon}`}></i>
+                  </span>
+                </div>
+                <h3 className="mb-5">{heading}</h3>
+                <p>{subHeading}</p>
+              </div>
             </div>
-            <h3 className="mb-5">Address</h3>
-            <p >198 West 21th Street, Suite 721 New York NY 10016</p>
-          </div>
-        </div>
-        <div className="col-md-6 col-lg-3 d-flex">
-          <div className="align-self-stretch box p-4  text-center">
-            <div className="contact__section-icon d-flex align-items-center justify-content-center">
-              <span className="icon-map-sign">
-                <i className="fas fa-phone"></i>
-              </span>
-            </div>
-            <h3 className="mb-5">contact</h3>
-            <p >+91 123456789</p>
-          </div>
-        </div>
-        <div className="col-md-6 col-lg-3 d-flex">
-          <div className="align-self-stretch box p-4  text-center">
-            <div className="contact__section-icon d-flex align-items-center justify-content-center">
-              <span className="icon-map-sign">
-                <i className="far fa-paper-plane"></i>
-              </span>
-            </div>
-            <h3 className="mb-5">email address</h3>
-            <p >support@trustcare.com</p>
-          </div>
-        </div>
-        <div className="col-md-6 col-lg-3 d-flex">
-          <div className="align-self-stretch box p-4  text-center">
-            <div className="contact__section-icon d-flex align-items-center justify-content-center">
-              <span className="icon-map-sign">
-                <i className="fas fa-globe-asia"></i>
-              </span>
-            </div>
-            <h3 className="mb-5">website</h3>
-            <p >trust@career.com</p>
-          </div>
-        </div>
+          );
+        })}
       </div>
     </div>
   );
