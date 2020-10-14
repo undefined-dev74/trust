b368d5fa1337b418db6c71bf3877e5e375a8370f
JuniorBaymax <cooladitya953@gmail.com>
2020-10-15 00:10:07 +0530

    Refector the Services section

--- src/components/Services.jsx
+++ src/components/Services.jsx
@@ -1,75 +1,71 @@
-import React, { Component } from "react";
+import React from "react";
 
-export default class Projects extends Component {
-  render() {
-    return (
-      <section className="container services">
-        <div className="row justify-content-center mt-5">
-          <div className="col-md-6 staff__heading-section  text-center ">
-            <span className="staff__subheading">Services</span>
-            <h2 className="mb-3 staff__heading">our services</h2>
-            <p className="mt-0 mb-5">
-              Combining in-depth industry knowledge and technological expertise,
-              our team provides world-class IT services and solutions for
-              companies of any size, from all across the globe.
-            </p>
-          </div>
-        </div>
-        <div className="row d-flex mt-5 mb-5 services-content">
-          <div className="col-lg-3 col-md-6 col-12 d-flex">
-            <div className="shadow__box">
-              <div className="shadow__box-icon">
-                <i className="fas fa-desktop"></i>
-              </div>
-              <h4 className="shadow__box-heading">Web Apps</h4>
-              <p className="shadow__box-subheading">
-                From a simple web app to a complex application that integrates
+const services = () => {
+  const contents = [
+    {
+      id: 1,
+      heading: "Web Apps",
+      subHeading: `'From a simple web app to a complex application that integrates
                 with a variety enterprise ERP services or 3rd party cloud
-                service we have got you covered.
-              </p>
-            </div>
-          </div>
-          <div className="col-lg-3 col-md-6 col-12 d-flex">
-            <div className="shadow__box ">
-              <div className="shadow__box-icon">
-                <i className="fas fa-drafting-compass"></i>
-              </div>
-              <h4 className="shadow__box-heading">App Development</h4>
-              <p className="shadow__box-subheading">
-                Create an impactful mobile app that fits your brand and industry
+                service we have got you covered.'`,
+      icon: "desktop",
+    },
+    {
+      id: 2,
+      heading: "App Development",
+      subHeading: `Create an impactful mobile app that fits your brand and industry
                 within a shorter time frame - whether you want iOS, Android or a
-                hybrid mobile application.
-              </p>
-            </div>
-          </div>
-          <div className="col-lg-3 col-md-6 col-12 d-flex">
-            <div className="shadow__box ">
-              <div className="shadow__box-icon">
-                <i className="fas fa-gamepad"></i>
-              </div>
-              <h4 className="shadow__box-heading">Game Development</h4>
-              <p className="shadow__box-subheading">
-                Create an impactful Game that fits your brand and industry
+                hybrid mobile application.`,
+      icon: "drafting-compass",
+    },
+    {
+      id: 3,
+      heading: "Game Development",
+      subHeading: ` Create an impactful Game that fits your brand and industry
                 within a shorter time frame - whether you want iOS, Android or a
-                hybrid mobile games.
-              </p>
-            </div>
-          </div>
-          <div className="col-lg-3 col-md-6 col-12 d-flex">
-            <div className="shadow__box ">
-              <div className="shadow__box-icon">
-                <i className="fab fa-opencart"></i>
-              </div>
-              <h4 className="shadow__box-heading">E-commerce Platform</h4>
-              <p className="shadow__box-subheading">
-                Business intelligence, maintenance & support, system integration
+                hybrid mobile games.`,
+      icon: "gamepad",
+    },
+    {
+      id: 4,
+      heading: "E-commerce Platform",
+      subHeading: `  Business intelligence, maintenance & support, system integration
                 and documentation - we have a full range of services to make
-                your e-commerce business a true success..
-              </p>
-            </div>
-          </div>
+                your e-commerce business a true success..`,
+      icon: "opencart",
+    },
+  ];
+
+  return (
+    <section className="container services">
+      <div className="row justify-content-center mt-5">
+        <div className="col-md-6 staff__heading-section  text-center ">
+          <span className="staff__subheading">Services</span>
+          <h2 className="mb-3 staff__heading">our services</h2>
+          <p className="mt-0 mb-5">
+            Combining in-depth industry knowledge and technological expertise,
+            our team provides world-class IT services and solutions for
+            companies of any size, from all across the globe.
+          </p>
         </div>
-      </section>
-    );
-  }
-}
+      </div>
+      <div className="row d-flex mt-5 mb-5 services-content">
+        {contents.map(({ id, heading, subHeading, icon }) => {
+          return (
+            <div key={id} className="col-lg-3 col-md-6 col-12 d-flex">
+              <div className="shadow__box ">
+                <div className="shadow__box-icon">
+                  <i className={`fas fa-${icon}`}></i>
+                </div>
+                <h4 className="shadow__box-heading">{heading}</h4>
+                <p className="shadow__box-subheading">{subHeading}</p>
+              </div>
+            </div>
+          );
+        })}
+      </div>
+    </section>
+  );
+};
+
+export default services;
