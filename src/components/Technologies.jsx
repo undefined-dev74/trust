7c87f6a52cd35b7c3c79e77489f03ed2b7e05fb5
JuniorBaymax <cooladitya953@gmail.com>
2020-10-14 13:40:29 +0530

    Added Expertise component to the App

---
+++ src/components/Technologies.jsx
@@ -0,0 +1,74 @@
+import React from "react";
+
+export default function Technologies() {
+  return (
+    <section className="technologies__banner pt-5 pt-md-0 pb-5 mb-5">
+      <div className="container">
+        <div className="row justify-content-center mb-4 pb-5">
+          <div className="col-md-6 staff__heading-section  text-center ">
+            <span className="staff__subheading">expertise</span>
+            <h2 className="mb-3 staff__heading">Our expertise</h2>
+            <p className="mt-0 mb-2">
+              Trust team has highly experienced professionals with advanced
+              technical skills.
+            </p>
+          </div>
+        </div>
+        <div className="row align-items-center pt-md-5 d-flex ">
+          <div className="col text-center mt-5 mt-md-0 ">
+            <div className="technologies__banner-icon">
+              <i className=" icon fab fa-python"></i>
+            </div>
+          </div>
+          <div className="col text-center mt-5 mt-md-0 ">
+            <div className="technologies__banner-icon">
+              <i className=" icon fab fa-vuejs"></i>
+            </div>
+          </div>
+          <div className="col text-center mt-5 mt-md-0 ">
+            <div className="technologies__banner-icon">
+              <i className=" icon fab fa-bootstrap"></i>
+            </div>
+          </div>
+          <div className="col text-center mt-5 mt-md-0 ">
+            <div className="technologies__banner-icon">
+              <i className=" icon fab fa-react"></i>
+            </div>
+          </div>
+          <div className="col text-center mt-5 mt-md-0 ">
+            <div className="technologies__banner-icon">
+              <i className=" icon fab fa-angular"></i>
+            </div>
+          </div>
+        </div>
+        <div className="row align-items-center pt-md-5 d-flex ">
+          <div className="col text-center mt-5 mt-md-0 ">
+            <div className="technologies__banner-icon">
+              <i className=" icon fab fa-figma"></i>
+            </div>
+          </div>
+          <div className="col text-center mt-5 mt-md-0 ">
+            <div className="technologies__banner-icon">
+              <i className=" icon fab fa-sketch"></i>
+            </div>
+          </div>
+          <div className="col text-center mt-5 mt-md-0 ">
+            <div className="technologies__banner-icon">
+              <i className=" icon fab fa-html5"></i>
+            </div>
+          </div>
+          <div className="col text-center mt-5 mt-md-0 ">
+            <div className="technologies__banner-icon">
+              <i className=" icon fab fa-css3"></i>
+            </div>
+          </div>
+          <div className="col text-center mt-5 mt-md-0 ">
+            <div className="technologies__banner-icon">
+              <i className=" icon fab fa-js-square"></i>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+}
